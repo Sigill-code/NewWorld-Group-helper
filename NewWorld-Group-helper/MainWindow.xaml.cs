ac2b364db9090ae07e77d6e8e5469cf2f213aa70
jimmy poulsen <poulsen1223@hotmail.dk>
2021-11-02 20:41:39 +0100

    Add project files.

---
+++ NewWorld-Group-helper/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewWorld_Group_helper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (Grid_New_Player.Visibility == Visibility.Visible)
+            {
+                Grid_Search_Player.Visibility = Visibility.Hidden;
+                Grid_New_Player.Visibility = Visibility.Hidden;
+                Grid_Search_Role.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                Grid_New_Player.Visibility = Visibility.Visible;
+                Grid_Search_Player.Visibility = Visibility.Hidden;
+                Grid_Search_Role.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void btn_Click(object sender, RoutedEventArgs e)
+        {
+            // add player info to db
+        }
+
+        private void btnSearchPlayer_Click(object sender, RoutedEventArgs e)
+        {
+            if (Grid_Search_Player.Visibility == Visibility.Visible)
+            {
+                Grid_New_Player.Visibility = Visibility.Hidden;
+                Grid_Search_Player.Visibility = Visibility.Hidden;
+                Grid_Search_Role.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                Grid_New_Player.Visibility = Visibility.Hidden;
+                Grid_Search_Player.Visibility = Visibility.Visible;
+                Grid_Search_Role.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void btnPlayerSearch_Click(object sender, RoutedEventArgs e)
+        {
+            // Search db for player info and print
+        }
+        
+        private void btnSearchRole_Click(object sender, RoutedEventArgs e)
+        {
+            if (Grid_Search_Role.Visibility == Visibility.Visible)
+            {
+                Grid_New_Player.Visibility = Visibility.Hidden;
+                Grid_Search_Player.Visibility = Visibility.Hidden;
+                Grid_Search_Role.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                Grid_New_Player.Visibility = Visibility.Hidden;
+                Grid_Search_Player.Visibility = Visibility.Hidden;
+                Grid_Search_Role.Visibility = Visibility.Visible;
+            }
+        }
+
+    }
+}
